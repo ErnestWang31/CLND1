aba425095e8ede7fab0e37d187d4135260b35163
Ernest Wang <ernestwang135791@gmail.com>
2024-03-17 16:52:32 -0400

    fw

---
+++ 2 Firmware/ftoa.h
@@ -0,0 +1,12 @@
+
+
+#ifndef FTOA_H_
+#define FTOA_H_
+
+#define MAX_MANTISA (1000)
+
+int ftoa (float x, char *str, char  prec, char format);
+
+
+
+#endif /* FTOA_H_ */
---
+++ 2 Firmware/utils.h
@@ -0,0 +1,17 @@
+
+
+/* this file contains generic utilities and functions */
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+
+double CubicInterpolate(
+   double y0,double y1,
+   double y2,double y3,
+   double mu);
+
+
+
+
+#endif /* UTILS_H_ */
